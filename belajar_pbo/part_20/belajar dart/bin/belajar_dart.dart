79746cf827eb6483051d9cc56a67079921bb5745
Yafett <81797546+Yafett@users.noreply.github.com>
2021-08-11 15:47:33 +0700

    Add files via upload

---
+++ belajar_pbo/part_20/belajar dart/bin/belajar_dart.dart
@@ -0,0 +1,13 @@
+//SOLID 1 and 2
+
+void main(List<String> arguments) {
+
+  /*
+  S - Single-responsiblity Principle
+  O - Open-closed Principle
+  L - Liskov Substitution Principle
+  I - Interface Segregation Principle
+  D - Dependency Inversion Principle
+   */
+
+}
