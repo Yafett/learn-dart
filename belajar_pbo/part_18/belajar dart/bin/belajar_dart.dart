04db4679f734bbf7d3bba2c476efc7199e46b382
Yafett <81797546+Yafett@users.noreply.github.com>
2021-08-11 11:54:50 +0700

    Add files via upload

---
+++ belajar_pbo/part_18/belajar dart/bin/belajar_dart.dart
@@ -0,0 +1,22 @@
+import 'package:meta/meta.dart';
+void main (List<String> arguments) {
+  Person p;
+
+  try {
+    p = Person(name : null);
+  } catch(e){
+    print(e);
+  }
+
+  print('hello');
+}
+
+class Person{
+  final String name;
+  final int age;
+
+  Person({@required this.name, this.age}) {
+    assert(name != null, "You must give the person's name");
+  }
+
+}
